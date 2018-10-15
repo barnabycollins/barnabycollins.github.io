a6d475b5e452335b69bae66887599eb4c3da5493
barnstorm3r <barnabyc@sky.com>
2018-10-15 23:35:29 +0100

    Updates to improve carousel :metal:

--- ticker/js/carousel.js
+++ ticker/js/carousel.js
@@ -1,9 +1,36 @@
-function CycleCarousel() {
-    $("#carousel1").css("left", "100vw");
-    setTimeout(comeBack, 2000);
-}
-function comeBack() {
-    $("#carousel1").css("left", "60vw");
+function BeginCarousel(num) {
+    numSlides = num;
+    currentNum = 0;
+    number = ''
+    number = currentNum.toString();
     setTimeout(CycleCarousel, 5000);
 }
-comeBack();
+function CycleCarousel() {
+    ShowShadow();
+}
+function ShowShadow() {
+    $("#carousel" + number).css("box-shadow", "0px 0px 1vw 0px #222222");
+    setTimeout(HideContent, 500);
+}
+function HideContent() {
+    $("#carousel" + number).css("left", "100vw");
+    setTimeout(SwapContent, 1000);
+}
+function SwapContent() {
+    currentNum += 1;
+    currentNum = currentNum % numSlides;
+    $("#carousel" + number).hide();
+    number = currentNum.toString();
+    $("#carousel" + number).css("left", "100vw");
+    $("#carousel" + number).css("box-shadow", "0px 0px 1vw 0px #222222");
+    $("#carousel" + number).show();
+    setTimeout(ShowContent, 1000);
+}
+function ShowContent() {
+    $("#carousel" + number).css("left", "60vw");
+    setTimeout(HideShadow, 700);
+}
+function HideShadow() {
+    $("#carousel" + number).css("box-shadow", "0px 0px 0px 0px #222222");
+    setTimeout(CycleCarousel, 5000);
+}
