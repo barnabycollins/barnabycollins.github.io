7f1321218bee8c985dd974bd2d2d6a0d4ecdbd13
Barnaby Collins <barnabyc@sky.com>
2019-02-07 12:42:50 +0000

    hell yeah now we have haikus

---
+++ poetry/haiku.js
@@ -0,0 +1,22 @@
+var haikuWords = [["On", "In", "With", "Surrounded by", "Eating", "Examining", "Poking", "Impressing", "Empowering"],
+                    ["cricket", "branch", "river", "dog", "cat", "character from an anime", "cheese", "computer", "body pillow"],
+                    ["floating", "feeding", "imagining", "drifting", "snoozing", "crying", "singing"],
+                    ["westwards", "downstream", "eastwards", "quickly", "smoothly", "emotionally", "slowly", "pensively"]];
+
+function chooseWord(n) {
+    return haikuWords[n][Math.floor(Math.random()*haikuWords[n].length)];
+}
+
+function generateHaiku() {
+    document.getElementById("ln-1").innerHTML = chooseWord(0) + " a " + chooseWord(1);
+    document.getElementById("ln-2").innerHTML = chooseWord(2) + " " + chooseWord(3);
+    document.getElementById("ln-3").innerHTML = "a " + chooseWord(1) + ", " + chooseWord(2) + ".";
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    generateHaiku();
+    document.getElementById("update").addEventListener("click", function(){
+        generateHaiku();
+    });
+});
+
