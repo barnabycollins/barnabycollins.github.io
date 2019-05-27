90db646e4e9ca81f81a62633f897a39c46e8ccea
barnstorm3r <barnabyc@sky.com>
2019-05-27 01:56:50 +0100

    Make toggle button change on click

--- ddjs/assets/djpage.js
+++ ddjs/assets/djpage.js
@@ -6,12 +6,17 @@ function toggleMusic(elem) {
     let musicDiv = $(elem).closest('div.dj-card').find('.dj-music');
     console.log($(musicDiv).css('margin-top'));
     if (musicDiv.css('margin-top') !== '0px') {
+        $(elem).html('Less');
         musicDiv.css('margin-top', '0px');
-        $('.dj-music').not(musicDiv).each((index, elem) => {
-            $(elem).css('margin-top', '-' + $(elem).outerHeight() + 'px');
+        $('.dj-music').not(musicDiv).each((index, el) => {
+            $(el).css('margin-top', '-' + $(el).outerHeight() + 'px');
         });
+        $('.music-expand').not(elem).each((index, el) => {
+            $(el).html('More');
+        })
     }
     else {
+        $(elem).html('More');
         musicDiv.css('margin-top', '-' + musicDiv.outerHeight() + 'px');
     }
 }
