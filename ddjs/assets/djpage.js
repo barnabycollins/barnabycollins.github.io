4879a16326449dc2e202ad5e74a6bf1a6d01c392
barnstorm3r <barnabyc@sky.com>
2019-05-27 21:28:34 +0100

    Add styled dropdown icons

--- ddjs/assets/djpage.js
+++ ddjs/assets/djpage.js
@@ -5,22 +5,27 @@ $('.dj-music').each((index, elem) => {
 function toggleMusic(elem) {
     let musicDiv = $(elem).closest('div.dj-card').find('.dj-music');
     if (musicDiv.css('margin-top') !== '0px') {
-        $(elem).html('Less');
+        $(elem).css('transform', 'translateY(-50%) rotate(180deg)');
         musicDiv.css('margin-top', '0px');
         $('.dj-music').not(musicDiv).each((index, el) => {
             $(el).css('margin-top', '-' + $(el).outerHeight() + 'px');
         });
         $('.music-expand').not(elem).each((index, el) => {
-            $(el).html('More');
+            $(el).css('transform', 'translateY(-50%) rotate(0deg)');
         })
     }
     else {
-        $(elem).html('More');
         musicDiv.css('margin-top', '-' + musicDiv.outerHeight() + 'px');
+        $(elem).css('transform', 'translateY(-50%) rotate(0deg)');
     }
 }
 
+function removeLoad() {
+    $('#load-cover').css('display', 'none');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    $('body').css('overflow-y', 'scroll')
+    $('body').css('overflow-y', 'scroll');
     $('#load-cover').css('opacity', '0');
-})
+    setTimeout(removeLoad, 2000);
+});
