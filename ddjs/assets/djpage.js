949f267c9d814bd16cb55f66134edc36337c0675
barnstorm3r <barnabyc@sky.com>
2019-06-03 00:54:21 +0100

    Content generates from JS object instead of static

--- ddjs/assets/djpage.js
+++ ddjs/assets/djpage.js
@@ -1,6 +1,40 @@
-$('.dj-music').each((index, elem) => {
-    $(elem).css('margin-top', '-' + $(elem).outerHeight() + 'px');
-});
+let djNames = shuffle(Object.keys(djs));
+let list = $('#dj-list');
+for (let i of djNames) {
+    let dj = djs[i];
+    list.append('<div class="dj-card"><div class="row"><div class="col-md-3 dj-img"><img></div><div class="col-md-8 dj-content"><h3></h3><div class="dj-bio"><p></p></div><ul class="genre-list"></ul></div><div class="col-md-1"><img src="assets/dropdown.svg" class="music-expand" onclick="toggleMusic(this)"></div></div><div class="dj-music-container"><div class="dj-music"></div></div></div>');
+    let card = $('#dj-list .dj-card:last-child');
+    dj.card = card;
+    card.find('.dj-content h3').html(i);
+    card.find('.dj-img img').attr('src', dj.img);
+    card.find('.dj-bio p').html(dj.bio);
+
+    for (let j of dj.genres) {
+        card.find('.genre-list').append('<li>' + j + '</li>')
+    }
+
+    for (j of dj.music) {
+        if (j[0] == 'mixcloud') {
+            card.find('.dj-music').append('<iframe height="120" scrolling="no" frameborder="0" source="https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=' + j[1] + '">Loading...</iframe>');
+        }
+        else if (j[0] == 'soundcloud') {
+            card.find('.dj-music').append('<iframe height="120" scrolling="no" frameborder="no" allow="autoplay" source="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + j[1] + '&color=%23a32691&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">Loading...</iframe>')
+        }
+    }
+    card.find('dj-music').css('margin-top', '-' + $(elem).outerHeight() + 'px');
+}
+
+window.onload = () => {
+    $('#load-cover').css('opacity', '0');
+    $('iframe').each((index, elem) => {
+        $(elem).attr('src', $(elem).attr('source'));
+    });
+    setTimeout(removeLoad, 1000);
+};
+
+function removeLoad() {
+    $('#load-cover').css('display', 'none');
+}
 
 function toggleMusic(elem) {
     let musicDiv = $(elem).closest('div.dj-card').find('.dj-music');
@@ -20,14 +54,21 @@ function toggleMusic(elem) {
     }
 }
 
-function removeLoad() {
-    $('#load-cover').css('display', 'none');
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+function shuffle(array) {
+	let currentIndex = array.length;
+	let temporaryValue, randomIndex;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
-
-window.onload = () => {
-    $('#load-cover').css('opacity', '0');
-    $('iframe').each((index, elem) => {
-        $(elem).attr('src', $(elem).attr('source'));
-    });
-    setTimeout(removeLoad, 2000);
-};
---
+++ ddjs/assets/djs.js
@@ -0,0 +1,27 @@
+let djs = {
+    'DJ Khaled': {
+        'bio': '<span class="khaled-deep-quote">"The price of fame, shit real. Fuck fame (man fuck this shit). Fuck the game."</span>  ~ DJ Khaled',
+        'img': 'assets/KHALED.jpg',
+        'genres': [
+            'memes',
+            'loud shouting'
+        ],
+        'music': [
+            ['soundcloud', '253565889'],
+            ['soundcloud', '253565840']
+        ]
+    },
+
+    'Stylish African American DJ Spinning Records': {
+        'bio': 'he stylish',
+        'img': 'assets/stylishman.jpg',
+        'genres': [
+            'jungle',
+            'garage',
+            'techno'
+        ],
+        'music': [
+            ['mixcloud', '/DJVadim/rumble-inna-mi-jungle/']
+        ]
+    }
+};
