36e437211a6aa21c091d001c4aa4f574e5909e84
Barnaby Collins <barnabyc@sky.com>
2021-08-26 10:02:43 +0100

    Add scrolling

---
+++ js/nav-highlight.js
@@ -0,0 +1,27 @@
+const sectionsToNavs = {
+    'welcome': 'welcome-link',
+    'skills': 'skills-link'
+}
+
+let previouslyFocussed = 'welcome';
+
+function checkScroll() {
+    const viewportX = document.documentElement.clientWidth;
+    const viewportY = document.documentElement.clientHeight;
+
+    const currentlyFocussed = document.elementsFromPoint(viewportX/2, viewportY/2);
+
+    for (let i = 0; i < currentlyFocussed.length; i++) {
+        const id = currentlyFocussed[i].id;
+        if (id !== previouslyFocussed && id in sectionsToNavs) {
+            document.getElementById(sectionsToNavs[previouslyFocussed]).style.fontWeight = 300;
+            document.getElementById(sectionsToNavs[id]).style.fontWeight = 400;
+            previouslyFocussed = id;
+            break;
+        }
+    }
+}
+
+document.addEventListener('scroll', checkScroll);
+
+checkScroll();
