56573f6a6f9c55f8ea396aa8f951460f447e96b8
Barnaby Collins <barnabyc@sky.com>
2021-09-02 16:29:51 +0100

    Stop nav highlighting happening on mobile

--- js/nav-highlight.js
+++ js/nav-highlight.js
@@ -1,22 +1,28 @@
-let previouslyFocussed = 'welcome';
 
-function checkScroll() {
-    const viewportX = document.documentElement.clientWidth;
-    const viewportY = document.documentElement.clientHeight;
+// If the navbar is visible
+if (!window.matchMedia("screen and (max-width: 800px)").matches) {
+    console.log("yes");
 
-    const currentlyFocussed = document.elementsFromPoint(viewportX/2, viewportY/2);
-
-    for (let i = 0; i < currentlyFocussed.length; i++) {
-        const id = currentlyFocussed[i].id;
-        if (id in sectionsToNavs && id !== previouslyFocussed) {
-            document.getElementById(sectionsToNavs[previouslyFocussed]).style.fontWeight = 300;
-            document.getElementById(sectionsToNavs[id]).style.fontWeight = 400;
-            previouslyFocussed = id;
-            break;
+    let previouslyFocussed = 'welcome';
+    
+    function checkScroll() {
+        const viewportX = document.documentElement.clientWidth;
+        const viewportY = document.documentElement.clientHeight;
+    
+        const currentlyFocussed = document.elementsFromPoint(viewportX/2, viewportY/2);
+    
+        for (let i = 0; i < currentlyFocussed.length; i++) {
+            const id = currentlyFocussed[i].id;
+            if (id in sectionsToNavs && id !== previouslyFocussed) {
+                document.getElementById(sectionsToNavs[previouslyFocussed]).style.fontWeight = 300;
+                document.getElementById(sectionsToNavs[id]).style.fontWeight = 400;
+                previouslyFocussed = id;
+                break;
+            }
         }
     }
+    
+    document.addEventListener('scroll', checkScroll);
+    
+    checkScroll();
 }
-
-document.addEventListener('scroll', checkScroll);
-
-checkScroll();
